cf19c450444cfd5ea3b3d4bf7dd5311713c536e5
br-ndt <nlax76@gmail.com>
2022-05-31 08:13:21 -0400

    modify socket endpoint to accommodate staging branch

--- client/src/services/socketEndpoint.js
+++ client/src/services/socketEndpoint.js
@@ -1,6 +1,8 @@
 const ENDPOINT =
 process.env.NODE_ENV === "development"
   ? "http://localhost:3000"
+  : process.env.STAGING
+  ? "https://murmuring-caverns-41303.herokuapp.com/"
   : "https://checker-io.herokuapp.com/";
 
   export default ENDPOINT;
